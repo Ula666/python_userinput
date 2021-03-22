3fa2a322fdc27c7bbb06e4ed48066eeadbfea5d0
rrose666 <uziolko@hotmail.com>
2021-03-22 17:04:29 +0000

    Changed the python_userinput.py file

---
+++ pyton_userinput.py
@@ -0,0 +1,29 @@
+first_name = input("What is your first name? ")
+
+last_name = input("What is your last name? ")
+
+age = int(input("How old are you? "))
+
+salary = float(input("What is your average salary per hour? "))
+
+date_of_birth = input("What is your date of birth? ")
+
+post_code = input("What is your post code? ")
+
+print(first_name)
+print(type(first_name))
+
+print(last_name)
+print(type(last_name))
+
+print(age)
+print(type(age))
+
+print(salary)
+print(type(salary))
+
+print(date_of_birth)
+print(type(date_of_birth))
+
+print(post_code)
+print(type(post_code))
